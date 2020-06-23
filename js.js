8e1ca1cd22dffa5aec7ed273b7a7680e4272515e
Olivia Helmuth <Olivia.Helmuth@pm.me>
2020-06-23 23:05:23 +0200

    start calculator logic and improve layout

---
+++ js.js
@@ -0,0 +1,52 @@
+new Vue({
+    el:"#vueCalc",
+    data:{
+        valArr: [],
+        previousVal: null,
+        currentVal: null,
+        operator: null,
+    },
+    methods:{
+        getValue(e) {
+            if(Number.isInteger(parseFloat(e.target.innerText)) || //check if its a number or a ,
+            e.target.innerText === ".") {
+                this.valArr.push(e.target.innerText);
+                this.currentVal = this.valArr.join(""); 
+            }
+            else {
+                this.previousVal = this.currentVal;
+                this.valArr = [];
+                this.currentVal = null;
+                this.operator = e.target.innerText;
+            }
+        },
+        compute() {
+            switch(this.operator) {
+                case "+":
+                    this.currentVal = parseFloat(this.previousVal) + parseFloat(this.currentVal);
+                    break;
+                case "-":
+                    this.currentVal = this.previousVal -  this.currentVal;
+                    break;
+                case "X":
+                    this.currentVal = this.previousVal * this.currentVal;
+                    break;
+                case "/":
+                    this.currentVal = this.previousVal / this.currentVal;
+                    break;
+                // the values are strings, but js automatically converts them into numbers while calculating, except with the + operator
+            };
+            // this.currentVal = this.currentVal.toString();
+            this.operator = "=";
+        },
+        clearAll() {
+            this.valArr = [],
+            this.previousVal = null,
+            this.currentVal = null,
+            this.operator = null
+        },
+        clearLastDigit() {
+            this.valArr.pop();
+        }
+    }
+})
